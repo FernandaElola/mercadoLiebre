20d94b13482a3402c02437e9aac010969bd08bf0
FernandaElola <elolafernanda@gmail.com>
2021-08-25 16:13:02 -0300

    subo formularios terminados

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
